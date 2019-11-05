7c4596a0e08418270f68ad965b4cad8b50325ff4
vebjorni <raulsanchezgonzalez1@gmail.com>
2019-11-05 11:15:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/proyecto007/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.proyecto007;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv1=(TextView) findViewById(R.id.tv1);
+    }
+    public void llamar(View view){
+        tv1.setText("Lammando...");
+    }
+}
